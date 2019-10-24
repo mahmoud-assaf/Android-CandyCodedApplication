310a935ade928adb2a54cb34c64fc58d905c37b8
mahmoud <twice.nice@yahoo.com>
2019-10-24 22:17:10 +0200

    t

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -94,9 +94,8 @@ public class MainActivity extends AppCompatActivity {
     }
 @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.info){
-            startActivity(new Intent(this,InfoActivity.class));
-        }
+        Intent infoIntent=new Intent(this,InfoActivity.class);
+	   startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
 
